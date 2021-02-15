4d8fbae2c8304a6304e4fdc2e7cf026b1c34f013
unknown <orionb.y23@gmail.com>
2021-02-15 02:26:57 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './screens/cart_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
-      //value: Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          //value: Products(),
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider(
+          //value: Cart(),
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
@@ -23,6 +33,7 @@ class MyApp extends StatelessWidget {
         home: ProductsOverviewScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
         },
       ),
     );
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,10 +5,14 @@ import '../providers/products.dart';
 import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
@@ -16,10 +20,10 @@ class ProductsGrid extends StatelessWidget {
         //create: (c) => products[i],
         value: products[i],
         child: ProductItem(
-        //  products[i].id,
-        //  products[i].title,
-        //  products[i].imageUrl,
-        ),
+            //  products[i].id,
+            //  products[i].title,
+            //  products[i].imageUrl,
+            ),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
