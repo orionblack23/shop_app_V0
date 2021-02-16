b05a1af50788ed1a84ed51f0a07c555f1c569dae
unknown <orionb.y23@gmail.com>
2021-02-16 16:42:32 +0000

    update create Form and TextFormField

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -56,6 +56,21 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added item to cart',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      }),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
