ed4951af46e9b259a68e26576c321e6b1fdd74b3
unknown <orionb.y23@gmail.com>
2021-02-19 00:12:21 +0000

    post orders data to database

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -10,6 +10,26 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final cart = Provider.of<Cart>(context);
+    var row = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Total',
+          style: TextStyle(fontSize: 20),
+        ),
+        Spacer(),
+        Chip(
+          label: Text(
+            '\$${cart.totalAmount.toStringAsFixed(2)}',
+            style: TextStyle(
+              color: Theme.of(context).primaryTextTheme.headline6.color,
+            ),
+          ),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        OrderButton(cart: cart),
+      ],
+    );
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Cart'),
@@ -20,37 +40,7 @@ class CartScreen extends StatelessWidget {
             margin: EdgeInsets.all(15),
             child: Padding(
               padding: EdgeInsets.all(8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Total',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Spacer(),
-                  Chip(
-                    label: Text(
-                      '\$${cart.totalAmount.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        color:
-                            Theme.of(context).primaryTextTheme.headline6.color,
-                      ),
-                    ),
-                    backgroundColor: Theme.of(context).primaryColor,
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      Provider.of<Orders>(context, listen: false).addOrder(
-                        cart.items.values.toList(),
-                        cart.totalAmount,
-                      );
-                      cart.clear();
-                    },
-                    child: Text('ORDER NOW'),
-                    textColor: Theme.of(context).primaryColor,
-                  ),
-                ],
-              ),
+              child: row,
             ),
           ),
           SizedBox(
@@ -70,6 +60,45 @@ class CartScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  _OrderButtonState createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+          ? null
+          : () async {
+              setState(() {
+                _isLoading = true;
+              });
+              await Provider.of<Orders>(context, listen: false).addOrder(
+                widget.cart.items.values.toList(),
+                widget.cart.totalAmount,
+              );
+              setState(() {
+                _isLoading = false;
+              });
+              widget.cart.clear();
+            },
+      child:_isLoading ? CircularProgressIndicator() : Text('ORDER NOW'),
+      textColor: Theme.of(context).primaryColor,
     );
   }
 }
