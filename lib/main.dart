145e4a412f3d7144d43520410b56f02372b6ed0b
unknown <orionb.y23@gmail.com>
2021-02-20 01:31:50 +0000

    wiring up sign up form

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import './screens/cart_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/orders_screen.dart';
+import './providers/auth.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
         ChangeNotifierProvider(
           //value: Products(),
           create: (ctx) => Products(),
